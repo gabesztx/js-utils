02ff4d8919ef387cc018876ddf4f455796a6f8aa
Gábor Martus <martus.gabor@nexius.hu>
2018-01-08 17:07:40 +0100

    add new nxkitchen

---
+++ nx-style-kitchen-sink/index.js
@@ -0,0 +1,3 @@
+import './src/index.scss';
+import 'jquery';
+import './node_modules/bootstrap/dist/js/bootstrap';
---
+++ nx-style-kitchen-sink/webpack.dev.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './index.js',
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            '$': 'jquery',
+            'jQuery': 'jquery',
+            'window.$': 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Nexius Style Kitchen Sink',
+            template: './src/index.ejs'
+        })
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+                'file-loader'
+            ]
+        }, {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: [
+                'file-loader'
+            ]
+        }]
+    }
+};
