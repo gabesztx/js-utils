892ddbee1d112aba1d3bc78cda29f66778e54950
Martus Gabor <gabesztx@gmail.com>
2018-04-09 22:08:20 +0200

    xy

--- angular5-webpack4-boilerplate/src/app/app.module.ts
+++ angular5-webpack4-boilerplate/src/app/app.module.ts
@@ -2,14 +2,20 @@ import {HttpClientModule} from '@angular/common/http';
 // import {AngularSvgIconModule} from 'angular-svg-icon';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppRootComponent} from './root-component/app-root.component';
-import {FooterComponent} from './footer-component/footer.component';
+// import {FooterComponent} from './footer-component/footer.component';
 
 @NgModule({
-	declarations: [AppRootComponent, FooterComponent], //FooterComponent
-	imports: [BrowserModule, HttpClientModule], //AngularSvgIconModule
-	bootstrap: [AppRootComponent]
+    declarations: [
+        AppRootComponent,
+        //FooterComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        //AngularSvgIconModule
+    ],
+    bootstrap: [AppRootComponent]
 })
 export class AppModule {
 }
