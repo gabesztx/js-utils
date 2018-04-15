ffcd851134e5947dbf491c37c21a74afadcf322a
Martus Gabor <gabesztx@gmail.com>
2018-04-15 08:46:04 +0200

    delete

--- angular-webpack/src/app/componenet-local-varible/countdown-parent/countdown-parent.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-countdown-parent',
-  templateUrl: './countdown-parent.component.html',
-  styleUrls: ['./countdown-parent.component.css']
-})
-export class CountdownParentComponent {}
