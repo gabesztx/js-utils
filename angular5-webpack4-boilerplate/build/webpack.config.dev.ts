38eccb7aff18124f274bf65b645184a079905497
Martus Gabor <gabesztx@gmail.com>
2018-04-15 06:40:03 +0200

    delete

--- angular5-webpack4-boilerplate/build/webpack.config.dev.ts
+++
@@ -1,5 +0,0 @@
-process.env.NODE_ENV = 'development';
-
-import {config} from './webpack.config';
-
-module.exports = config;
--- angular5-webpack4-boilerplate/src/vendor.ts
+++
@@ -1,5 +0,0 @@
-// Angular
-import '@angular/core';
-import '@angular/platform-browser';
-import '@angular/platform-browser-dynamic';
-import '@angular/common/http';
