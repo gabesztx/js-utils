5bf93151210db968c37bdebbac5c5b506bdb24dc
gabesz <gabesztx@gmail.com>
2018-11-25 20:23:46 +0100

    learning changeEvent

--- ngrx-learn/src/app/app.component.ts
+++ ngrx-learn/src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterContentInit {
   config = {
     position: 'top'
   };
+
+  ngOnInit() {
+    // console.log('ngOnInit');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+    setTimeout(() => {
+      console.log('Trigger config');
+      this.config = {
+        position: 'bottom'
+      };
+    }, 3000);
+  }
 
   onClick() {
     console.log('click');
--- ngrx-learn/src/app/components/main-game/main-game.component.ts
+++ ngrx-learn/src/app/components/main-game/main-game.component.ts
@@ -1,47 +1,89 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Input, ChangeDetectionStrategy, ChangeDetectorRef, Component, OnChanges, OnInit, AfterContentInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import * as fromRoot from '../../reducers/index.reducer';
+import * as controllerReducer from '../../reducers/controller.reducer';
+import * as controllerAction from '../../actions/controller.action';
 
 @Component({
   selector: 'app-main-game',
   templateUrl: './main-game.component.html',
   styleUrls: ['./main-game.component.scss'],
-  // changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MainGameComponent implements OnInit, OnChanges {
-  // @Input() name: string;
-  // @Input() config: any;
-  count = 0;
-  // controller$: Observable<controllerReducer.State>;
+export class MainGameComponent implements OnInit, OnChanges, AfterContentInit {
+  @Input() config: any;
+  controller$: Observable<controllerReducer.State>;
+  value = 0;
+  otherValue = 0;
+  conf: string;
 
-  constructor(private store: Store<fromRoot.MainState>,
-              private cdr: ChangeDetectorRef) {
-    setInterval(() => {
-      this.count++;
-      // this.cdr.detectChanges();
-    }, 1000);
+  constructor(private store: Store<fromRoot.MainState>, private cdr: ChangeDetectorRef) {
   }
 
   ngOnChanges() {
-    // console.log('Changes');
+    console.log('ngOnChanges', this.config);
+    // this.conf = this.config.position;
   }
 
   ngOnInit() {
+    console.log('ngOnInit');
+    /*setTimeout(() => {
+      console.log('Trigger 10 value');
+      this.value = 10;
+      this.cdr.detectChanges();
+    }, 1000);*/
+    // this.controller$ = this.store.pipe(select(fromRoot.getControlller));
+
   }
 
-  get runChangeDetection1() {
-    console.log('Checking the view - 1');
-    return 'string 1';
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+    this.controller$ = this.store.pipe(select(fromRoot.getControlller));
+    // this.controller$.subscribe((res) => { this.value += 1000; });
+    setTimeout(() => {
+      this.addValue();
+    }, 2000);
   }
 
-  get runChangeDetection2() {
-    console.log('Checking the view - 2');
-    return 'string 2';
+
+  addValue() {
+    console.log('addValue');
+    // this.store.dispatch(new controllerAction.AddCounter());
+    this.otherValue = 5000;
   }
 
-  add() {
-    // console.log('ADDD');
-    // this.count++;
+  removeValue() {
+    console.log('removeValue');
+    // this.store.dispatch(new controllerAction.RemoveCounter());
+    // this.cdr.detectChanges();
+
   }
+
+  get triggerVal1() {
+    console.log('triggerVal  -   1');
+    return 'triggerVal1';
+  }
+
+  get triggerVal2() {
+    console.log('triggerVal  -   2');
+    return 'triggerVal2';
+  }
+
+
 }
 
+
+// this.controller$.subscribe();
+/*
+ngAfterContentInit() {
+  let num = 0;
+  setInterval(() => {
+    if (num < 5) {
+      this.addValue();
+    } else {
+      this.removeValue();
+    }
+    num++;
+  }, 500);
+}*/
