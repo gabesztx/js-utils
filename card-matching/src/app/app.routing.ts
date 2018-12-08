2f7236fb91819891cb90e9e46449e282ddb98843
gabesz <gabesztx@gmail.com>
2018-12-08 23:57:53 +0100

    add bg image

--- card-matching/src/app/app.routing.ts
+++ card-matching/src/app/app.routing.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
-// import { PreloadGuard } from './shared/guards/preload.guard';
 import { LandingPageComponent } from './view/start/components/landing-page/landing-page.component';
 import { GamePageComponent } from './view/game/components/game-page/game-page.component';
+// import { PreloadGuard } from './shared/guards/preload.guard';
 
 
 export const routes: Routes = [
-  {path: 'start', component: LandingPageComponent},
-  {path: 'game', component: GamePageComponent},
+  {
+    path: 'start',
+    component: LandingPageComponent
+  },
+  {
+    path: 'game',
+    component: GamePageComponent
+  },
   {path: '', redirectTo: 'start', pathMatch: 'full'},
   {path: '**', redirectTo: 'start', pathMatch: 'full'},
 ];
