f862d0b87c5c75dbd48c1c7371c3c7e7557f1ced
gabesz <gabesztx@gmail.com>
2018-12-12 19:48:55 +0100

    done done

--- card-matching/src/app/reducers/card.reducer.ts
+++ card-matching/src/app/reducers/card.reducer.ts
@@ -8,7 +8,7 @@ export interface IState {
 
 const initial_state: IState = {
   cards: [],
-  deckSize: 7
+  deckSize: 5
 };
 
 export function reducer(state = initial_state, action: CardAction.Actions) {
