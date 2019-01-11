6b429337ac514277327eb4bb489f3eb2ed143173
gabesz <gabesztx@gmail.com>
2019-01-11 22:25:23 +0100

    reset

--- ngrx/src/app/app.module.ts
+++ ngrx/src/app/app.module.ts
@@ -8,9 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { reducers, metaReducers } from './reducers';
 import { environment } from '../environments/environment';
-import { GameModule } from './game/game.module';
-import * as fromGame from './game/reducers/game.reducer';
-import { GameEffects } from './game/effects/game.effects';
+
 
 @NgModule({
   declarations: [
@@ -22,9 +20,6 @@ import { GameEffects } from './game/effects/game.effects';
     StoreModule.forRoot(reducers, { metaReducers }),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     EffectsModule.forRoot([]),
-    GameModule,
-    StoreModule.forFeature('game', fromGame.reducer),
-    EffectsModule.forFeature([GameEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
