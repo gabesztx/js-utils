2105bc1d5328c5a7267f52226448afd31749d351
gabesz <gabesztx@gmail.com>
2019-01-11 23:12:30 +0100

    add features

--- ngrx/src/app/game/game.module.ts
+++ ngrx/src/app/game/game.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GameRoutingModule } from './game-routing.module';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './reducers/game.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { GameEffects } from './effects/game.effects';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    GameRoutingModule
+    GameRoutingModule,
+    StoreModule.forFeature('game', reducer),
+    EffectsModule.forFeature([GameEffects])
   ]
 })
-export class GameModule { }
+export class GameModule {
+}
--- ngrx/src/app/reducers/index.ts
+++ ngrx/src/app/reducers/index.ts
@@ -6,10 +6,15 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import * as fromGame from '../game/reducers/game.reducer';
 
-export interface State {}
+export interface State {
+  game: fromGame.State;
+}
 
-export const reducers: ActionReducerMap<State> = {};
+export const reducers: ActionReducerMap<State> = {
+  game: fromGame.reducer,
+};
 
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
