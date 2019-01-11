aaa4d753b0ff45bc0e840b8bea7178029cb34696
gabesz <gabesztx@gmail.com>
2019-01-11 23:44:49 +0100

    wip

--- ngrx/src/app/game/game.module.ts
+++ ngrx/src/app/game/game.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { GameRoutingModule } from './game-routing.module';
 import { StoreModule } from '@ngrx/store';
 import { reducers } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { GameEffects } from './effects/game.effects';
 
 @NgModule({
   declarations: [],
@@ -10,6 +12,7 @@ import { reducers } from './reducers';
     CommonModule,
     GameRoutingModule,
     StoreModule.forFeature('game', reducers),
+    EffectsModule.forFeature([GameEffects]),
   ]
 })
 export class GameModule {
--- ngrx/src/app/game/reducers/index.ts
+++ ngrx/src/app/game/reducers/index.ts
@@ -5,11 +5,14 @@ import {
   createSelector,
   MetaReducer
 } from '@ngrx/store';
+import * as fromGame from './game.reducer';
 
 export interface State {
 
+  game: fromGame.State;
 }
 
 export const reducers: ActionReducerMap<State> = {
 
+  game: fromGame.reducer,
 };
