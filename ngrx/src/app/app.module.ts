0e3de71665f6f73f8ab12dbad8dea1cd8b62c9af
gabesz <gabesztx@gmail.com>
2019-01-11 22:12:15 +0100

    add

--- ngrx/src/app/app.module.ts
+++ ngrx/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { AppComponent } from './app.component';
 import { reducers, metaReducers } from './reducers';
 import { environment } from '../environments/environment';
 import { GameModule } from './game/game.module';
+import * as fromGame from './game/reducers/game.reducer';
+import { GameEffects } from './game/effects/game.effects';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { GameModule } from './game/game.module';
     StoreModule.forRoot(reducers, { metaReducers }),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     EffectsModule.forRoot([]),
-    GameModule
+    GameModule,
+    StoreModule.forFeature('game', fromGame.reducer),
+    EffectsModule.forFeature([GameEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
