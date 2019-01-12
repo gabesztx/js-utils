3225bc7f57a1e05f725f1aaf716ecd9a1e3ae8d8
gabesz <gabesztx@gmail.com>
2019-01-12 23:24:02 +0100

    working progress

--- ngrx/src/app/app.module.ts
+++ ngrx/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
-import { GameModule } from './game/game.module';
 
 import { environment } from '../environments/environment';
 import { reducers } from './reducers';
@@ -21,7 +20,6 @@ import { AppComponent } from './app.component';
     StoreModule.forRoot(reducers),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     EffectsModule.forRoot([]),
-    GameModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
