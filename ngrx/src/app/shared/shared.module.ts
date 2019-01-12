6a674d50eedb53054398964467d2790b94869338
gabesz <gabesztx@gmail.com>
2019-01-12 23:40:18 +0100

    wip

--- ngrx/src/app/shared/shared.module.ts
+++ ngrx/src/app/shared/shared.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
-import { reducers } from './reducers';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    StoreModule.forFeature('shared', reducers)
   ]
 })
 export class SharedModule {
