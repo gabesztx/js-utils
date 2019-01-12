70c583d665d71635eec6b6fe27e29ebc750d57ae
gabesz <gabesztx@gmail.com>
2019-01-12 23:49:30 +0100

    assets

--- ngrx/src/app/shared/shared.module.ts
+++ ngrx/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StoreModule } from '@ngrx/store';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
     CommonModule,
-  ]
+  ],
+  exports: [HeaderComponent]
 })
 export class SharedModule {
 }
