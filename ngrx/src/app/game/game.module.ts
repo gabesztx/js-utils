2264cec8505e8cedda5417c7d4200f976e791704
gabesz <gabesztx@gmail.com>
2019-01-13 01:05:56 +0100

    wip

--- ngrx/src/app/game/game.module.ts
+++ ngrx/src/app/game/game.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { GameEffects } from './effects/game.effects';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forFeature('game', reducers),
+    EffectsModule.forFeature([GameEffects])
   ]
 })
-export class GameModule { }
+export class GameModule {
+}
