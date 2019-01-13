70f00d32203c43f006579292dd74a4f09e694bf6
gabesz <gabesztx@gmail.com>
2019-01-13 01:20:09 +0100

    wip

--- ngrx/src/app/game/effects/game.effects.ts
+++ ngrx/src/app/game/effects/game.effects.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { GameActionTypes } from '../actions/game.actions';
+// import { GameActionTypes } from '../actions/game.actions';
 
 @Injectable()
 export class GameEffects {
 
   @Effect()
-  loadGames$ = this.actions$.pipe(ofType(GameActionTypes.LoadGames));
+  // loadGames$ = this.actions$.pipe(ofType(GameActionTypes.LoadGames));
 
   constructor(private actions$: Actions) {}
 }
