7d6141b44f31f7e7a4e694a4b8d11c0212bf5722
gabesz <gabesztx@gmail.com>
2019-02-10 07:08:21 +0100

    wip

--- angular-sandbox/src/app/ad/components/item3/item3.component.ts
+++ angular-sandbox/src/app/ad/components/item3/item3.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { AdComponent } from '../../models/ad.model';
 
 @Component({
-  selector: 'app-item3',
   templateUrl: './item3.component.html',
   styleUrls: ['./item3.component.scss']
 })
--- angular-sandbox/src/app/ad/containers/ad-page/ad-page.component.ts
+++ angular-sandbox/src/app/ad/containers/ad-page/ad-page.component.ts
@@ -5,7 +5,7 @@ import * as fromAd from '../../reducers';
 @Component({
   selector: 'app-ad-page',
   templateUrl: './ad-page.component.html',
-  styleUrls: ['./ad-page.component.css']
+  styleUrls: ['./ad-page.component.scss']
 })
 export class AdPageComponent implements OnInit {
 
