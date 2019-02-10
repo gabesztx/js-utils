087fa0e6c928bdd0f444e4ba2bc462fe50651c72
gabesz <gabesztx@gmail.com>
2019-02-10 21:02:54 +0100

    wip

--- angular-sandbox/src/app/ui/ui.module.ts
+++ angular-sandbox/src/app/ui/ui.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    LayoutComponent
   ]
 })
-export class UiModule { }
+export class UiModule {
+}
