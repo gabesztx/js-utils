87b33512ee9cac526b7447bcdc88bbab0a2613b8
Martus Gabor <gabesztx@gmail.com>
2019-02-23 04:06:24 +0100

    click evet

--- polymer-lit-html/src/lit-html/writing-templates/event-listeners/index.js
+++ polymer-lit-html/src/lit-html/writing-templates/event-listeners/index.js
@@ -1,6 +1,21 @@
 import { html, render } from 'lit-html';
+
+/*const myTemplate = (event) => html`<button @click=${event.handleEvent}>Click Me!</button>`;
+const clickHandler = {
+  // handleEvent method is required.
+  handleEvent(e) {
+    console.log('clicked!', e);
+  },
+  // event listener objects can also define zero or more of the event
+  // listener options: capture, passive, and once.
+  capture: true,
+};
+render(myTemplate(clickHandler), document.body);*/
+
+
 const myTemplate = () => html`<button @click=${clickHandler}>Click Me!</button>`;
 const clickHandler = () => {
   console.log('Click');
 };
-render(myTemplate(), document.body);
+render(myTemplate(clickHandler), document.body);
+
