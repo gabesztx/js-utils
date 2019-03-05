2eb6648aefc17b90dc28822f238df585239abf6f
Martus Gabor <gabesztx@gmail.com>
2019-03-05 16:43:56 +0100

    wip

--- sp-sandbox/src/index.js
+++ sp-sandbox/src/index.js
@@ -1,13 +1,7 @@
-import './index.css'
+import './index.css';
 // import { App, AppComponent } from './lite-element';
 // import { MainContainer } from "./lite-element/components/main-container";
 
-// customElements.define('app-root', AppComponent);
-// customElements.define('main-container', MainContainer);
-// new App();
-module.hot.accept('./lite-element', (obj) => {
-  // console.log('Hot');
-  // $('app-root').remove();
-  // new App();
-});
+// module.hot.accept('./lite-element', () => {});
+
 // TODO: templattel or shadow dom change when use hotModule
