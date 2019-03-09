fbaa83e0307ab04be1546a6860f694d343794b6d
gabesz <gabesztx@gmail.com>
2019-03-09 09:06:12 +0100

    svg

--- gallows/src/app/app.module.ts
+++ gallows/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
-
 import { environment } from '../environments/environment';
 import { reducers } from './reducers';
 import { AppComponent } from './app.component';
