8349fc8982477c7ad701ae29021c9f496a631a37
gabesz <gabesztx@gmail.com>
2019-03-23 12:08:38 +0100

    input change done

--- sp-gallowsgame/src/app/game/containers/game-page/game-page.component.ts
+++ sp-gallowsgame/src/app/game/containers/game-page/game-page.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromStore from '../../reducers';
 
 @Component({
   selector: 'app-game-page',
   templateUrl: './game-page.component.html',
-  styleUrls: ['./game-page.component.css']
+  styleUrls: ['./game-page.component.scss']
 })
 export class GamePageComponent implements OnInit {
+  private pattern = /^[A-Za-z]*$/;
 
-  constructor(private store: Store<fromStore.State>) { }
+  public word = 'SUPERCHARGE';
+  public letterArr: Array<string> = [];
 
-  ngOnInit() {
-    console.log('GAME PAGE');
+  @ViewChild('letter') inputLetter: ElementRef;
+  constructor(private store: Store<fromStore.State>) {
+    this.letterArr = this.word.split('');
   }
 
+  ngOnInit() {
+    this.inputLetter.nativeElement.focus();
+  }
+
+  onChange(event) {
+    if (!this.pattern.test(event.key)) {
+      event.preventDefault();
+    }
+    event.target.value = '';
+  }
 }
--- sp-gallowsgame/src/app/game/game.module.ts
+++ sp-gallowsgame/src/app/game/game.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
@@ -13,6 +14,7 @@ import * as fromGame from './reducers';
   imports: [
     CommonModule,
     GameRoutingModule,
+    FormsModule,
     StoreModule.forFeature('game', fromGame.reducers),
     EffectsModule.forFeature([GameEffects])
   ],
