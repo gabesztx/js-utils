a88745a8f15189e30aea3e8347c286136dec264c
gabesz <gabesztx@gmail.com>
2019-03-23 03:11:23 +0100

    add landing

--- sp-gallowsgame/src/app/game/landing-page/landing-page.component.ts
+++ sp-gallowsgame/src/app/game/landing-page/landing-page.component.ts
@@ -1,19 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Router } from '@angular/router';
+import { select, Store } from '@ngrx/store';
 import * as fromGameStore from '../../reducers';
+// import { SetDeckSize } from '../../actions/card.actions';
+// import { GameEffects } from '../../effects/game.effects';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
-
-  constructor(private store: Store<fromGameStore.State>) {
+  // deckSize$: Observable<number>;
+  constructor(
+    private store: Store<fromGameStore.State>,
+    // private gameEffects: GameEffects,
+    private router: Router) {
   }
 
   ngOnInit() {
-
+    // this.deckSize$ = this.store.pipe(select(fromGame.getDeckSize));
   }
 
+  startGame() {
+    // this.router.navigate(['game']);
+  }
+
+  changeDeckSize(value) {
+    // this.store.dispatch(new SetDeckSize(value));
+  }
 }
