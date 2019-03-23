7a5b28bb85bf870d6f5854697c07dcd3a02b4d58
gabesz <gabesztx@gmail.com>
2019-03-23 02:07:37 +0100

    wip

--- sp-gallowsgame/src/app/game/game.module.ts
+++ sp-gallowsgame/src/app/game/game.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import * as fromGame from './reducers/game.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { GameEffects } from './effects/game.effects';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forFeature('game', fromGame.reducer),
+    EffectsModule.forFeature([GameEffects])
   ]
 })
 export class GameModule { }
--- sp-gallowsgame/src/app/reducers/index.ts
+++ sp-gallowsgame/src/app/reducers/index.ts
@@ -6,13 +6,16 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import * as fromGame from '../game/reducers/game.reducer';
 
 export interface State {
 
+  game: fromGame.State;
 }
 
 export const reducers: ActionReducerMap<State> = {
 
+  game: fromGame.reducer,
 };
 
 
