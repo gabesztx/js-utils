ff64afbd5d78de514609d028c1b9f9e5acbe18a6
Martus Gabor <gabesztx@gmail.com>
2019-03-28 15:54:34 +0100

    wip

--- sp-sandbox/src/date-range/index.js
+++ sp-sandbox/src/date-range/index.js
@@ -8,42 +8,38 @@ export class DateRange {
     //  const rangeEvery = d3.timeDay.every(2).range(start, end);
     const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    // const dayDiffNum = d3.timeDay.count(startDay, endDay);
 
-    const startDay = new Date(2019, 2, 17);
-    const endDay = new Date(2019, 2, 27);
+    const startDay = new Date(2019, 2, 24);
+    const endDay = new Date(2019, 2, 31);
     const diffDay = d3.timeDay.range(startDay, endDay);
-    const diffWeek = d3.timeWeek.range(startDay, endDay);
-    console.log(diffWeek);
-    // TODO heti row alapján legenárlni az oszlopokat
+    diffDay.push(endDay);
 
-    // diffDay.push(endDay);
-    // console.log('Days:', diffDay);
-    //
-    // console.log('diffWeek:', diffWeek);
-
-   /* const days = diffDay.length;
-    const date1 = new Date('2019-03-11');
-    const date2 = new Date('2019-03-31');
-    const getWeek = function (d1, d2) {
-      const ONE_WEEK = 1000 * 60 * 60 * 24 * 7;
-      const date1 = d1.setDate(d1.getDate() - 1);
-      const date1_ms = new Date(date1).getTime();
-      // const date1_ms = d1.getTime();
-      const date2_ms = d2.getTime();
-      // console.log('Date 1', date1_ms);
-      const difference_ms = Math.abs(date1_ms - date2_ms);
-      return Math.floor(difference_ms / ONE_WEEK);
-
-    };*/
-    // console.log(date1);
-    // console.log(date1.getDate() + 1);
-    // console.log(getWeek(date1, date2) + 1);
-    // alert(Math.round(dif/1000/60/60/24/7));
-    // const diffMonth = d3.timeMonth.range(startDay, endDay);
-    // console.log(diffMonth);
-
-
+    const diffWeek = d3.timeWeek.range(startDay, endDay).length;
+    const dateContent = d3.select('.dateContent').node();
+    
+    for (let i = 0; i <= diffWeek; i++) {
+      let row = document.createElement('div');
+      row.setAttribute('class', 'dateItemRow');
+      dateContent.appendChild(row);
+      for (let j = 0; j <= 6; j++) {
+        let item = document.createElement('div');
+        item.setAttribute('class', 'dateItem');
+        row.appendChild(item);
+      }
+    }
+    const dateItemRows = d3.selectAll('.dateItemRow').nodes();
+    let rowId = 0;
+    for (let k = 0; k < diffDay.length; k++) {
+      const time = diffDay[k];
+      const date = time.getDate();
+      const day = time.getDay();
+      const dayId = day === 0 ? 7 : day;
+      const dateRow = dateItemRows[rowId].children[dayId-1];
+      if(dayId === 7){
+        rowId++;
+      }
+      dateRow.innerText = date;
+    }
   }
 }
 
--- sp-sandbox/src/index.js
+++ sp-sandbox/src/index.js
@@ -2,8 +2,6 @@ import './index.scss';
 // import { StacketBulletChart } from './d3';
 import { DateRange } from './date-range';
 new DateRange();
-/*
-module.hot.accept('./date-range', () => {
-  // new DateRange();
-});
-*/
+// module.hot.accept('./date-range', () => {
+//   new DateRange();
+// });
