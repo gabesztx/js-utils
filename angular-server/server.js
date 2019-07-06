9ea43ee333f6be03770b8487d8b9cfe31bb064d2
Gabesz <gabesztx@gmail.com>
2019-07-07 01:02:58 +0200

    wip

--- angular-server/server.js
+++ angular-server/server.js
@@ -19,3 +19,24 @@ server.listen(port, () => console.log('server is running!'));
 
 // app.get('/get', (req, res) => {res.send('GET')});
 // app.get('/', (req, res) => res.sendFile(path.join(__dirname)));
+
+
+/*
+const API_KEY = '58ff6da10a2ab4510611b2d6dafcb165-2b0eef4c-3e3d7643';
+const DOMAIN = 'gabesztx@gmail.com';
+const mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
+
+const sendMail = () => {
+  const data = {
+    from: 'Zita&Gabesz <zitugabesz@hazasodunk.hu>',
+    to: 'gabesztx@gmail.com',
+    subject: 'Zitu és Gabesz wedding',
+    text: 'Köszi hogy jössz, várunk szeretettel! :)'
+  };
+
+  mailgun.messages().send(data, (error, body) => {
+    console.log('OK!');
+    console.log(body);
+  });
+
+};*/
