44978c2bb06803846c118e3c3b3c3a33e0cd4a76
Gabesz <gabesztx@gmail.com>
2019-09-14 18:56:58 +0200

    wip

--- rtc-angular/src/app/user-media/components/basic-media/basic-media.component.ts
+++ rtc-angular/src/app/user-media/components/basic-media/basic-media.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+'use strict';
+
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-basic-media',
   templateUrl: './basic-media.component.html',
   styleUrls: ['./basic-media.component.scss']
 })
-export class BasicMediaComponent implements OnInit {
+export class BasicMediaComponent implements AfterViewInit {
+  @ViewChild('videoElement', {static: false}) vidRef: ElementRef;
+  video: HTMLVideoElement;
+  constraints = {
+    audio: false,
+    video: true
+  };
 
-  constructor() { }
+  constructor() {
+  }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.video = this.vidRef.nativeElement;
+  }
+
+  handleSuccess(stream: MediaStream) {
+    // (window as any).stream = stream;
+    this.video.srcObject = stream; // append media stream, then start video camera or audio
+    const videoTracks = stream.getVideoTracks(); // video metadata
+    const audioTracks = stream.getAudioTracks(); // audio metadata
+    // console.log('videoTracks' videoTracks);
+    // console.log('audioTracks' videoTracks);
+  }
+
+  async getMedia() {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia(this.constraints);
+      this.handleSuccess(stream);
+    } catch (err) {
+      console.log('Error Media: ', err);
+    }
+  }
+
+  startCamera() {
+    this.getMedia();
   }
 
 }
