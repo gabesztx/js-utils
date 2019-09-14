9830f8e7d25a1f07264dd25d97818b4e9e41b839
Gabesz <gabesztx@gmail.com>
2019-09-14 16:00:06 +0200

    wip

---
+++ rtc-angular/src/app/user-media/components/basic-media/basic-media.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-media',
+  templateUrl: './basic-media.component.html',
+  styleUrls: ['./basic-media.component.scss']
+})
+export class BasicMediaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
