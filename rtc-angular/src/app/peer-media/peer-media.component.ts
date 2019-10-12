fe291dc0f3a4617ce66ecce7d77a1cd16313deab
gabor.martus <gabor.martus@sigmatechnology.se>
2019-10-12 17:30:37 +0200

    wip

--- rtc-angular/src/app/peer-media/peer-media.component.ts
+++ rtc-angular/src/app/peer-media/peer-media.component.ts
@@ -34,7 +34,8 @@ export class PeerMediaComponent implements OnInit, AfterViewInit {
   stream: any;
 
 
-  constructor() {}
+  constructor() {
+  }
 
   ngAfterViewInit() {
     this.video = this.vidRef.nativeElement;
@@ -42,82 +43,102 @@ export class PeerMediaComponent implements OnInit, AfterViewInit {
 
 
   ngOnInit() {
+    // this.peerId = this.isChrome ? 'streamer' : 'clients';
     this.peerId = this.isChrome ? 'streamer' : 'clients';
     this.peer = new Peer(this.peerId, {host: 'localhost', port: 9000, path: '/'});
-    setTimeout(() => {
-      if (!this.isChrome) {
-        // this.getMediaStream();
-      }
+    this.peer.on('open', (id) => {
+    });
+    this.peer.on('close', () => {
+      console.log('close peer')
     });
 
     this.peer.on('call', (call) => {
-      // console.log('call from :', call);
-      call.answer(this.stream);
-      call.on('stream', (streamData) => {
-        console.log('call stream:', streamData);
-        this.video.srcObject = streamData;
+      call.answer();
+      call.on('stream', (mediaStream) => {
+        console.log('stream from server:', mediaStream);
+        this.video.srcObject = mediaStream;
       });
-      // call.on('close', () => {});
+      call.on('close', () => {
+        console.log('close Client media stream');
+      });
+      /*  this.getMediaStream().then(() => {
+          // console.log('peer call', call);
+          call.answer(this.stream);
+          // console.log('peer answer');
+          call.on('stream', (mediaStream) => {
+            console.log('Stream from server:', mediaStream);
+            this.video.srcObject = mediaStream;
+          });
+          call.on('close', () => {
+            console.log('Close Client media stream');
+            // this.video.srcObject = streamData;
+          });
+        });*/
+
+      // setTimeout(() => {}, 3000);
+
     });
-    // this.peer.on('open', (id) => {});
-    this.peer.on('close', () => {
-      console.log('Peer Event: close');
+    this.peer.on('connection', (data) => {
+      console.log('connection:', data);
+
     });
+
     this.peer.on('disconnected', () => {
-      console.log('Peer Event: disconnected');
+      console.log('disconnect');
     });
 
-
+    this.peer.on('error', (err) => {
+      console.log('Error', err);
+    });
   }
 
-
   startStream() {
+    // console.log('startStream');
     this.getMediaStream().then(() => {
-      this.startCamera();
+      this.video.srcObject = this.stream;
       this.mediaConnection = this.peer.call('clients', this.stream);
+      this.mediaConnection.on('stream', (stream) => {
+        console.log('stream for client', stream);
+        // setTimeout(() => {}, 3000);
+      });
+      this.mediaConnection.on('close', () => {
+        console.log('close server mediaConnection');
+      });
+      // this.mediaConnection.close();
     });
     // console.log();
     /*  if (!this.stream) {
         console.log('go go go');
         this.getMediaStream();
       }*/
+    // this.startCamera();
     // if (this.isChrome) {}
-    // this.mediaConnection.on('open', () => {});
-    // this.mediaConnection.on('close', () => {});
+    // this.peerCall.on('open', () => {});
+    // this.peerCall.on('close', () => {});
   }
 
-
   stopStream() {
-    if (!this.mediaConnection) {
-      return;
-    }
-    // console.log('stopStream');
-    this.mediaConnection.close();
-    this.mediaConnection = null;
-    this.stopCamera();
+    console.log('stopStream');
+    // this.mediaConnection.close();
+    // this.peer.disconnect();
+    this.peer.destroy();
+
+    /* video element off */
     this.stream.getTracks().forEach((track) => {
       track.stop();
     });
-    this.stream = null;
+    this.video.srcObject = null;
+
+    // this.stream = null;
+    // this.peerCall = null;
+    // this.stopCamera();
     // this.peer.disconnect();
     // this.video.srcObject = null;
     // this.video.srcObject = this.stream; // append media stream, then start video camera or audio
     // if (!this.isFirefox) {}
   }
 
-  startCamera() {
-    this.video.srcObject = this.stream; // append media stream, then start video camera or audio
-
-  }
-
-  stopCamera() {
-    this.video.srcObject = null;
-    // if (!this.isFirefox) {}
-  }
-
-
   handleSuccess(stream: MediaStream) {
-    console.log('go stream');
     this.stream = stream;
     // const videoTracks = stream.getVideoTracks(); // video data
     // const audioTracks = stream.getAudioTracks(); // audio data
@@ -126,7 +147,6 @@ export class PeerMediaComponent implements OnInit, AfterViewInit {
   }
 
   async getMediaStream() {
-
     try {
       const stream = await navigator.mediaDevices.getUserMedia(this.constraints);
       this.handleSuccess(stream);
@@ -134,4 +154,23 @@ export class PeerMediaComponent implements OnInit, AfterViewInit {
       console.log('Error Media: ', err);
     }
   }
+
+  /* startCamera() {
+    console.log('startCamera');
+    this.video.srcObject = this.stream; // append media stream, then start video or audio
+
+  }
+
+  stopCamera() {
+    console.log('stopCamera');
+    this.video.srcObject = null;
+  }
+*/
 }
+
+
+/* setTimeout(() => {
+   if (!this.isChrome) {
+     // this.getMediaStream();
+   }
+ });*/
