5d1be712e4060438002bbd17aac00624f8368201
Martus Gabor <gabesztx@gmail.com>
2019-11-25 00:37:29 +0100

    server init

---
+++ media-stream/server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const server = require('http').createServer(app);
+const port = 9000;
+const appFolder = '../dist/media-stream/';
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, appFolder, '/index.html'));
+});
+app.use(express.static(path.join(__dirname, appFolder)));
+
+server.listen(port, () => {
+  console.log('Server ' + app.name + ' listening on http://localhost:' + port)
+});
+
