abd9c4cfd82a79559ae632b74f8d2c4f1af5d714
Martus Gabor <gabesztx@gmail.com>
2019-12-04 08:29:47 +0100

    wip

---
+++ rtc-connect/src/server/index.ts
@@ -0,0 +1,23 @@
+// import * as fs from 'fs';
+import * as express from 'express';
+import * as http from 'http';
+import * as path from 'path';
+
+const app = express();
+const server = http.createServer(app);
+
+const appPath = '../../dist/rtc-connection/';
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, appPath, '/index.html'));
+});
+app.use(express.static(path.join(__dirname, appPath)));
+
+server.listen(port, () => {});
+
+/* Socket server */
+const io = require('socket.io')(server);
+io.on('connection', (socket) => {
+  console.log(socket);
+});
