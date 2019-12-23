e2096c3c20b7248b6dd142872951700a93aaf216
Martus Gabor <gabesztx@gmail.com>
2019-12-23 17:49:21 +0100

    ws socekt

--- rtc-connect/server/app.ts
+++ rtc-connect/server/app.ts
@@ -12,19 +12,19 @@ const wss = new WebSocket.Server({server});
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, config.rootPath, '/index.html')));
 app.use(express.static(path.join(__dirname, config.rootPath)));
+server.listen(config.serverPort, () => {
+});
 // socket.listen(server);
 
 wss.on('connection', (ws: WebSocket) => {
-  console.log('connection');
-  // connection is up, let's add a simple simple event
-  ws.on('message', (message: string) => {
-    // log the received message and send it back to the client
-    // console.log('received: %s', message);
-    // ws.send(`Hello, you sent -> ${message}`);
+  ws.on('message', (message) => {
+    wss.clients.forEach((client) => {
+      if (ws !== client && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
   });
-  // send immediatly a feedback to the incoming connection
-  // ws.send('Hi there, I am a WebSocket server');
+  // ws.on('close', () => {});
 });
 
-server.listen(config.serverPort, () => {
-});
+
--- rtc-connect/src/app/components/https/https.component.ts
+++ rtc-connect/src/app/components/https/https.component.ts
@@ -8,21 +8,43 @@ import { SocketService } from '../../services/socket.service';
   styleUrls: ['./https.component.scss']
 })
 export class HttpsComponent implements OnInit {
+  ws: WebSocket;
 
   // constructor() {}
-  constructor(private wsSocketService:) {
+  constructor() {
   }
 
   ngOnInit() {
-    const socket = new WebSocket('wss://192.168.1.23:3000');
+    this.connect();
     // this.socketService.getSandBox().subscribe((msg) => {
     //   console.log('Get Socket Msg: ', msg);
     // });
   }
 
+  connect() {
+    this.ws = new WebSocket('wss://192.168.1.23:3000');
+    this.ws.onopen = (event) => {
+      console.log('WS: Open');
+    };
+    this.ws.onclose = (event) => {
+      console.log('WS: Close');
+      this.ws = null;
+      setTimeout(() => {
+        this.connect();
+      }, 1500);
+    };
+    this.ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      console.log('WS: Message: ', data);
+    };
+  }
+
   send() {
-    // const data = 'Hello' + ' - ' + Math.random() * 1000;
-    // this.socketService.sendSandBox(data);
+    const msg = {
+      channel: 'message',
+      data: 'Hello' + ' - ' + Math.random() * 1000
+    };
+    this.ws.send(JSON.stringify(msg));
   }
 
 }
