1ce389c4330a9dc6872749451c7664db67b7b299
Gabesz <gabesztx@gmail.com>
2020-04-18 10:38:19 +0200

    teszt1

--- angular-sandbox/src/app/user/components/user/user.component.ts
+++ angular-sandbox/src/app/user/components/user/user.component.ts
@@ -7,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
-    console.log('Hello :)');
+    
   }
 
 }
