de2332af82e551ab269b8ea6e0e43bd1581338b0
Martus Gábor <gabesztx@gmail.com>
2020-04-23 15:11:23 +0200

    wip

--- angular-sandbox/src/app/forms/forms.module.ts
+++ angular-sandbox/src/app/forms/forms.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsComponent } from './forms.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
-import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
+import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
 
 @NgModule({
   declarations: [
     FormsComponent,
     ReactiveFormComponent,
-    DynamicFormComponent
+    DynamicFormComponent,
   ],
   imports: [
     CommonModule,
