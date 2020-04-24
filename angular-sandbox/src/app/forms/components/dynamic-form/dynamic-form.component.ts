233a0c35f2da848d71ac346e8435664ae99ecd59
Martus Gábor <gabesztx@gmail.com>
2020-04-24 20:16:39 +0200

    wip

--- angular-sandbox/src/app/forms/components/dynamic-form/dynamic-form.component.ts
+++ angular-sandbox/src/app/forms/components/dynamic-form/dynamic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-form',
@@ -7,20 +7,42 @@ import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
   styleUrls: ['./dynamic-form.component.scss']
 })
 export class DynamicFormComponent implements OnInit {
-  EMAIL_LIST = ['teszt1@test.hu', 'teszt2@test.hu', 'teszt3@test.hu'];
-  emailListForm: FormGroup;
+  emailListData = ['teszt1@test.hu', 'teszt2@test.hu'];
+  form: FormGroup;
+  // emailListData = [];
 
-  constructor(private fb: FormBuilder) {
-    this.emailListForm = this.fb.group({
-
-    })
+  get emailList() {
+    return this.form.get('emailList') as FormArray;
   }
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.createGroup();
+    this.form.addControl('emailList', this.createArray());
+    if (this.emailListData.length) {
+      this.emailListData.forEach(() => this.addEmail());
+    }
+    // console.log(this.form);
   }
 
-  createEmails(){
-
+  createGroup(): FormGroup {
+    return this.fb.group({});
   }
 
+  createControl(): FormControl {
+    return this.fb.control('', Validators.required);
+  }
+
+  createArray(): FormArray {
+    return this.fb.array([]);
+  }
+
+  addEmail() {
+    this.emailList.push(this.createControl());
+  }
+
+  onSubmit() {
+    console.log(this.emailList);
+  }
 }
