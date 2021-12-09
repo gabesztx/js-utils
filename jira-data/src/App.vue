cbe1027b117a175c36e0a6640af2efdfc815e2a1
Martus Gábor <gabor.martus@icellmobilsoft.hu>
2021-12-09 19:21:48 +0100

    delete

--- jira-data/src/App.vue
+++ jira-data/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-<!--  <div id="nav"></div>-->
-<!--    <router-link to="/">Home</router-link> |-->
-<!--    <router-link to="/about">About</router-link>-->
+  <!--  <div id="nav"></div>-->
+  <!--    <router-link to="/">Home</router-link> |-->
+  <!--    <router-link to="/about">About</router-link>-->
 
   <router-view />
 </template>
--- jira-data/src/components/HelloWorld.vue
+++ jira-data/src/components/HelloWorld.vue
@@ -1,17 +1,48 @@
 <template>
   <div class="hello">
-    Hello
+    <button @click="onClick">Get Data</button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import axios from "axios";
 
 export default defineComponent({
   name: "HelloWorld",
   props: {
-    msg: String,
+    msg: String
   },
+  setup(){
+    const token = "NTUwNjgxNjY5MjQyOuFqi06FiytL7SrgsV3tiV35AJij";
+    const config = {
+      headers: {
+        // "Access-Control-Allow-Origin": "*",
+        Authorization: `Bearer ${token}`,
+        Accept: "application/json"
+      }
+    };
+    const GET_ISSUE = "rest/agile/1.0/issue/HUGODEV-16298";
+    const GET_ALL_BOARD = "rest/agile/1.0/board";
+    const GET_BOARD = "rest/agile/1.0/board/229";
+    const GET_ISSUES_FOR_BOARD = "rest/agile/1.0/board/229/issue";
+    // const GET_PROJETCS = "rest/agile/1.0/board/229/project";
+    const GET_PROJETCS = "rest/api/2/project/10602";
+    const host = `http://localhost:8080/${GET_PROJETCS}`;
+    const onClick = () => {
+      axios.get(host, config).then(
+        (response) => {
+          console.log(response.data);
+        },
+        () => {
+          console.log("ERROR");
+        }
+      );
+    };
+    return {
+      onClick
+    };
+  }
 });
 </script>
 
@@ -20,14 +51,17 @@ export default defineComponent({
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
